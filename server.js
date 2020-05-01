2093bf55704010c0e7d7c11199801055d7d360a3
leonelyo99 <carocirenzo@gmail.com>
2020-05-01 13:51:10 -0300

    paginas dinamicas a node

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('./node_modules/express'); //requiero express
-const app = express(); //instancio express
+const express = require('express');
+const path = require('path');
+const app = express();
 
-//obteniendo el puerto de la pagina 
-const port = process.env.PORT;
-// const port = 3000;
+//obteniendo el puerto de la pagina
+// const port = process.env.PORT;
+const port = 3000;
 
-//midelware donde se mostrara esto, creando un dominio publicpo
-app.use(express.static( __dirname + '/public' )); 
- 
-app.listen(port, ()=>{ //el puerto que esta escuchando
-    console.log(`Escuchando desde el puerto ${port}`);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(port, () => {
+  //el puerto que esta escuchando
+  console.log(`Escuchando desde el puerto ${port}`);
 });

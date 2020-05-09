e29758a493345e2e869547019988e297c86da170
leonelyo99 <carocirenzo@gmail.com>
2020-05-09 20:27:08 -0300

    add compression al server

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const compresión = require("compresión");
+const compression = require('compression')
 
 //obteniendo el puerto de la pagina
 const port = process.env.PORT;
 // const port = 3000;
 
-app.use(compression({ filter: shouldCompress }))
- 
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false
-  }
- 
-  // fallback to standard filter function
-  return compression.filter(req, res)
-}
+app.use(compression());
 
 app.use(express.static(__dirname + "/public"));
 
